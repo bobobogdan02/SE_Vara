d444bc9b50e0929a7970bc1926fbdaf83e7087f4
Bogdan Vieru <bogdanvieru98@gmail.com>
2020-07-05 22:19:31 +0300

    Models

---
+++ Classroom/Models/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classroom.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
---
+++ Classroom/Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classroom.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        
+        public string Teacher { get; set; }
+    }
+}
